fc0cfd0066923e47b986291ebd0390da39653519
kingwill101 <hey@glenfordwilliams.com>
2024-07-20 17:47:16 -0500

    changelog  + bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+
+## 0.2.0
+
+- Add support for checking if sockets have data available
+
+
 ## 0.1.0
 
 - Initial version.
--- example/bin/example.dart
+++ example/bin/example.dart
@@ -5,7 +5,11 @@ import 'package:native_socket/native_socket.dart';
 void main(List<String> arguments) {
   final socket = UnixSocket('/run/user/1000/wayland-1');
   socket.send(Uint8List.fromList([1, 2, 3]));
-  final data = socket.receive();
-  print(data);
+
+  if (socket.hasData()) {
+    final data = socket.receive();
+    print(data);
+  }
+
   socket.close();
 }
