84056da3c02beca62449b146cd3241f7460f2588
kingwill101 <hey@glenfordwilliams.com>
2026-07-14 01:06:15 -0500

    Expand C native_socket API with socket ops, fd passing, datagram, options
    Add new C functions:
    - create_socket, bind_socket, bind_socket_abstract, listen_socket, accept_socket
    - connect_unix_socket, connect_unix_socket_abstract
    - send_to, send_to_abstract, recv_from
    - set_so_sndbuf, get_so_sndbuf, set_so_rcvbuf, get_so_rcvbuf, set_so_linger
    - unlink_socket_path, create_tmpfile_cloexec, os_create_anonymous_file
    - socket_has_data, recv_fd helper
    - send_bytes_with_fd for SCM_RIGHTS fd passing
    - Internal helpers: set_cloexec_or_close, c_msg_len, c_msg_space

--- src/native_socket.c
+++ src/native_socket.c
@@ -1,5 +1,336 @@
-
+#define _GNU_SOURCE
 #include "native_socket.h"
+#include <sys/select.h>
+#include <unistd.h>
+#include <errno.h>
+
+// ---------------------------------------------------------------------------
+// Internal helpers (defined first to avoid forward-declaration issues)
+// ---------------------------------------------------------------------------
+
+static int set_cloexec_or_close(int fd)
+{
+    long flags;
+
+    if (fd == -1)
+        return -1;
+
+    flags = fcntl(fd, F_GETFD);
+    if (flags == -1)
+        goto err;
+
+    if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
+        goto err;
+
+    return fd;
+
+err:
+    close(fd);
+    return -1;
+}
+
+int create_tmpfile_cloexec(char *tmpname)
+{
+    int fd;
+
+#ifdef HAVE_MKOSTEMP
+    fd = mkostemp(tmpname, O_CLOEXEC);
+    if (fd >= 0)
+        unlink(tmpname);
+#else
+    fd = mkstemp(tmpname);
+    if (fd >= 0)
+    {
+        fd = set_cloexec_or_close(fd);
+        unlink(tmpname);
+    }
+    else
+    {
+        printf("mkstemp error: %s\n", strerror(errno));
+    }
+#endif
+
+    return fd;
+}
+
+size_t c_msg_len(size_t datalen)
+{
+    return CMSG_LEN(datalen);
+}
+
+size_t c_msg_space(size_t datalen)
+{
+    return CMSG_SPACE(datalen);
+}
+
+// ---------------------------------------------------------------------------
+// SCM_RIGHTS helpers
+// ---------------------------------------------------------------------------
+
+/// Receive a single file descriptor from a Unix socket via SCM_RIGHTS.
+int recv_fd(int socket)
+{
+    struct msghdr msg = {0};
+    char m_buffer[256];
+    struct iovec io = {.iov_base = m_buffer, .iov_len = sizeof(m_buffer)};
+
+    char c_buffer[c_msg_space(sizeof(int))];
+    memset(c_buffer, 0, sizeof(c_buffer));
+
+    msg.msg_iov = &io;
+    msg.msg_iovlen = 1;
+    msg.msg_control = c_buffer;
+    msg.msg_controllen = c_msg_space(sizeof(int));
+
+    if (recvmsg(socket, &msg, 0) < 0)
+    {
+        printf("recvmsg error: %s\n", strerror(errno));
+        return -1;
+    }
+
+    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
+
+    int fd;
+    if (cmsg && cmsg->cmsg_len == c_msg_len(sizeof(int)))
+    {
+        if (cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SCM_RIGHTS)
+        {
+            fd = *((int *)CMSG_DATA(cmsg));
+            return fd;
+        }
+    }
+
+    return -1;
+}
+
+/// Encode file descriptors into a socket control message buffer.
+void *unix_rights(int *fds, int num_fds, unsigned char *buf, size_t buflen)
+{
+    struct cmsghdr *cm = (struct cmsghdr *)buf;
+    if (c_msg_space(num_fds * sizeof(int)) > buflen)
+    {
+        return NULL;
+    }
+    cm->cmsg_len = c_msg_len(num_fds * sizeof(int));
+    cm->cmsg_level = SOL_SOCKET;
+    cm->cmsg_type = SCM_RIGHTS;
+    memcpy(CMSG_DATA(cm), fds, num_fds * sizeof(int));
+    return buf;
+}
+
+/// Decode file descriptors from a socket control message.
+int parse_unix_rights(struct cmsghdr *cm, int *fds, int max_fds)
+{
+    if (cm->cmsg_level != SOL_SOCKET || cm->cmsg_type != SCM_RIGHTS)
+    {
+        return -1;
+    }
+    int num_fds = (cm->cmsg_len - CMSG_LEN(0)) / sizeof(int);
+    num_fds = num_fds > max_fds ? max_fds : num_fds;
+    if (cm->cmsg_len != c_msg_len(num_fds * sizeof(int)))
+    {
+        return -1;
+    }
+    memcpy(fds, CMSG_DATA(cm), num_fds * sizeof(int));
+    return num_fds;
+}
+
+// ---------------------------------------------------------------------------
+// Socket creation
+// ---------------------------------------------------------------------------
+
+/// Create a pair of connected socket file descriptors (like socketpair syscall).
+/// The sockets are set to non-blocking and CLOEXEC.
+/// Returns 0 on success, -1 on error. sv[0] and sv[1] are filled on success.
+int create_socketpair(int sv[2])
+{
+    int ret = socketpair(AF_UNIX, SOCK_STREAM | SOCK_CLOEXEC | SOCK_NONBLOCK, 0, sv);
+    if (ret < 0) {
+        printf("socketpair error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+// -------------------------------------------------------------------------
+// Socket creation and lifecycle
+// -------------------------------------------------------------------------
+
+int create_socket(int type)
+{
+    int sockfd = socket(AF_UNIX, type | SOCK_CLOEXEC | SOCK_NONBLOCK, 0);
+    if (sockfd == -1) {
+        printf("socket error: %s\n", strerror(errno));
+        return -1;
+    }
+    return sockfd;
+}
+
+int bind_socket(int socket, const char *path)
+{
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+
+    if (bind(socket, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        printf("bind error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int bind_socket_abstract(int socket, const char *name)
+{
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    // Abstract namespace: first byte of sun_path is null byte
+    // We copy the name AFTER the leading null
+    size_t name_len = strlen(name);
+    if (name_len > sizeof(addr.sun_path) - 1) {
+        name_len = sizeof(addr.sun_path) - 1;
+    }
+    memcpy(addr.sun_path + 1, name, name_len);
+    // Total sockaddr size: offset of sun_path + 1 (null) + name length
+    socklen_t addr_len = offsetof(struct sockaddr_un, sun_path) + 1 + name_len;
+
+    if (bind(socket, (struct sockaddr *)&addr, addr_len) == -1) {
+        printf("bind abstract error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int listen_socket(int socket, int backlog)
+{
+    if (listen(socket, backlog) == -1) {
+        printf("listen error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int accept_socket(int socket)
+{
+    int client_fd = accept4(socket, NULL, NULL, SOCK_CLOEXEC | SOCK_NONBLOCK);
+    if (client_fd == -1) {
+        printf("accept error: %s\n", strerror(errno));
+        return -1;
+    }
+    return client_fd;
+}
+
+int unlink_socket_path(const char *path)
+{
+    if (unlink(path) == -1 && errno != ENOENT) {
+        printf("unlink error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+// -------------------------------------------------------------------------
+// Socket options
+// -------------------------------------------------------------------------
+
+int get_socket_option(int socket, int level, int optname, int *value)
+{
+    socklen_t len = sizeof(int);
+    if (getsockopt(socket, level, optname, (void *)value, &len) == -1) {
+        printf("getsockopt error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int set_socket_option(int socket, int level, int optname, int value)
+{
+    if (setsockopt(socket, level, optname, (void *)&value, sizeof(value)) == -1) {
+        printf("setsockopt error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int set_so_sndbuf(int socket, int size)
+{
+    return set_socket_option(socket, SOL_SOCKET, SO_SNDBUF, size);
+}
+
+int get_so_sndbuf(int socket)
+{
+    int value = 0;
+    if (get_socket_option(socket, SOL_SOCKET, SO_SNDBUF, &value) == -1) {
+        return -1;
+    }
+    return value;
+}
+
+int set_so_rcvbuf(int socket, int size)
+{
+    return set_socket_option(socket, SOL_SOCKET, SO_RCVBUF, size);
+}
+
+int get_so_rcvbuf(int socket)
+{
+    int value = 0;
+    if (get_socket_option(socket, SOL_SOCKET, SO_RCVBUF, &value) == -1) {
+        return -1;
+    }
+    return value;
+}
+
+int set_so_linger(int socket, int on_off, int linger_secs)
+{
+    struct linger l;
+    l.l_onoff = on_off;
+    l.l_linger = linger_secs;
+    if (setsockopt(socket, SOL_SOCKET, SO_LINGER, (void *)&l, sizeof(l)) == -1) {
+        printf("setsockopt SO_LINGER error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+// -------------------------------------------------------------------------
+// Unix socket (client connect)
+// -------------------------------------------------------------------------
+
+int connect_unix_socket(int socket, const char *path)
+{
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+
+    if (connect(socket, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        printf("connect error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+int connect_unix_socket_abstract(int socket, const char *name)
+{
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+
+    size_t name_len = strlen(name);
+    if (name_len > sizeof(addr.sun_path) - 1) {
+        name_len = sizeof(addr.sun_path) - 1;
+    }
+    memcpy(addr.sun_path + 1, name, name_len);
+    socklen_t addr_len = offsetof(struct sockaddr_un, sun_path) + 1 + name_len;
+
+    if (connect(socket, (struct sockaddr *)&addr, addr_len) == -1) {
+        printf("connect abstract error: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
 int create_unix_socket(const char *path)
 {
     int sockfd;
@@ -67,42 +398,7 @@ ssize_t send_bytes_with_fd(int socket, int fd, const void *data, size_t data_len
         return -1;
     }
 
-    return sent;  // Return the number of bytes sent
-}
-
-int recv_fd(int socket)
-{
-    struct msghdr msg = {0};
-    char m_buffer[256];
-    struct iovec io = {.iov_base = m_buffer, .iov_len = sizeof(m_buffer)};
-
-    char c_buffer[c_msg_space(sizeof(int))]; // Use c_msg_space function
-    memset(c_buffer, 0, sizeof(c_buffer));
-
-    msg.msg_iov = &io;
-    msg.msg_iovlen = 1;
-    msg.msg_control = c_buffer;
-    msg.msg_controllen = c_msg_space(sizeof(int)); // Use c_msg_space function
-
-    if (recvmsg(socket, &msg, 0) < 0)
-    {
-        printf("recvmsg error: %s\n", strerror(errno));
-        return -1;
-    }
-
-    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
-
-    int fd;
-    if (cmsg && cmsg->cmsg_len == c_msg_len(sizeof(int))) // Use c_msg_len function
-    {
-        if (cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SCM_RIGHTS)
-        {
-            fd = *((int *)CMSG_DATA(cmsg));
-            return fd;
-        }
-    }
-
-    return -1;
+    return sent;
 }
 
 ssize_t send_bytes(int socket, const void *buffer, size_t length)
@@ -122,17 +418,66 @@ ssize_t send_bytes(int socket, const void *buffer, size_t length)
 
 ssize_t recv_bytes(int socket, void *buffer, size_t length)
 {
-    ssize_t read = recv(socket, buffer, length, 0);
-    if (read == -1)
+    ssize_t n = recv(socket, buffer, length, 0);
+    if (n < 0)
     {
-        return 0;
+        return -1; // propagate error to caller
     }
-    return read;
+    return n; // 0 means clean close, >0 means bytes read
 }
 
 void close_socket(int socket)
 {
     close(socket);
+}
+
+// -------------------------------------------------------------------------
+// Datagram send/recv
+// -------------------------------------------------------------------------
+
+ssize_t send_to(int socket, const char *path, const void *data, size_t data_len)
+{
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path) - 1);
+
+    ssize_t sent = sendto(socket, data, data_len, 0, (struct sockaddr *)&addr, sizeof(addr));
+    if (sent < 0) {
+        printf("sendto error: %s\n", strerror(errno));
+        return -1;
+    }
+    return sent;
+}
+
+ssize_t send_to_abstract(int socket, const char *name, const void *data, size_t data_len)
+{
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+
+    size_t name_len = strlen(name);
+    if (name_len > sizeof(addr.sun_path) - 1) {
+        name_len = sizeof(addr.sun_path) - 1;
+    }
+    memcpy(addr.sun_path + 1, name, name_len);
+    socklen_t addr_len = offsetof(struct sockaddr_un, sun_path) + 1 + name_len;
+
+    ssize_t sent = sendto(socket, data, data_len, 0, (struct sockaddr *)&addr, addr_len);
+    if (sent < 0) {
+        printf("sendto abstract error: %s\n", strerror(errno));
+        return -1;
+    }
+    return sent;
+}
+
+ssize_t recv_from(int socket, void *buffer, size_t length)
+{
+    ssize_t n = recvfrom(socket, buffer, length, 0, NULL, NULL);
+    if (n < 0) {
+        return -1;
+    }
+    return n;
 }
 
 /*
@@ -188,108 +533,14 @@ int os_create_anonymous_file(off_t size)
     return fd;
 }
 
-static int set_cloexec_or_close(int fd)
-{
-    long flags;
-
-    if (fd == -1)
-        return -1;
-
-    flags = fcntl(fd, F_GETFD);
-    if (flags == -1)
-        goto err;
-
-    if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
-        goto err;
-
-    return fd;
-
-err:
-    close(fd);
-    return -1;
-}
-
-static int create_tmpfile_cloexec(char *tmpname)
-{
-    int fd;
-
-#ifdef HAVE_MKOSTEMP
-    fd = mkostemp(tmpname, O_CLOEXEC);
-    if (fd >= 0)
-        unlink(tmpname);
-#else
-    fd = mkstemp(tmpname);
-    if (fd >= 0)
-    {
-        fd = set_cloexec_or_close(fd);
-        unlink(tmpname);
-    }
-    else
-    {
-        printf("mkstemp error: %s\n", strerror(errno));
-    }
-#endif
-
-    return fd;
-}
-
-// Function to calculate the length of a control message with data length
-static size_t c_msg_len(size_t datalen)
-{
-    return CMSG_LEN(datalen);
-}
-
-// Function to calculate the total space needed for a control message with data length
-static size_t c_msg_space(size_t datalen)
-{
-    return CMSG_SPACE(datalen);
-}
-
-// Function to encode file descriptors into a socket control message
-void *unix_rights(int *fds, int num_fds, unsigned char *buf, size_t buflen)
-{
-    struct cmsghdr *cm = (struct cmsghdr *)buf;
-    if (c_msg_space(num_fds * sizeof(int)) > buflen)
-    {
-        return NULL; // Ensure buffer is large enough
-    }
-    cm->cmsg_len = c_msg_len(num_fds * sizeof(int)); // Use c_msg_len function
-    cm->cmsg_level = SOL_SOCKET;
-    cm->cmsg_type = SCM_RIGHTS;
-    memcpy(CMSG_DATA(cm), fds, num_fds * sizeof(int));
-    return buf;
-}
-
-// Function to decode file descriptors from a socket control message
-int parse_unix_rights(struct cmsghdr *cm, int *fds, int max_fds)
-{
-    if (cm->cmsg_level != SOL_SOCKET || cm->cmsg_type != SCM_RIGHTS)
-    {
-        return -1; // Error
-    }
-    int num_fds = (cm->cmsg_len - CMSG_LEN(0)) / sizeof(int);
-    num_fds = num_fds > max_fds ? max_fds : num_fds;
-    if (cm->cmsg_len != c_msg_len(num_fds * sizeof(int)))
-    {
-        return -1; // Length mismatch error
-    }
-    memcpy(fds, CMSG_DATA(cm), num_fds * sizeof(int));
-    return num_fds;
-}
-
-
-int write_to_fd(int fd, const unsigned char *buffer, size_t count) {
+ssize_t write_to_fd(int fd, const unsigned char *buffer, size_t count) {
     ssize_t result = write(fd, buffer, count);
     if (result == -1) {
         return errno;  // Return errno to handle specific error cases in Dart
     }
-    return result;  // Return the number of bytes written
+    return result;
 }
 
-
-#include <sys/select.h>
-#include <unistd.h>
-#include <errno.h>
 
 /**
  * Checks if the socket has available data to read.
--- src/native_socket.h
+++ src/native_socket.h
@@ -8,19 +8,103 @@
 #include <errno.h>
 #include <fcntl.h>
 
-static int create_tmpfile_cloexec(char *tmpname);
-int os_create_anonymous_file(off_t size);
+// ---------------------------------------------------------------------------
+// File / anonymous memory
+// ---------------------------------------------------------------------------
 
-int write_to_fd(int fd, const unsigned char *buffer, size_t count);
-int socket_has_data(int socket, int timeout);
-int create_unix_socket(const char *path);
+int create_tmpfile_cloexec(char *tmpname);
+int os_create_anonymous_file(off_t size);
+ssize_t write_to_fd(int fd, const unsigned char *buffer, size_t count);
+
+// ---------------------------------------------------------------------------
+// Socket types
+// ---------------------------------------------------------------------------
+
+/// Create a socket of the given type (SOCK_STREAM or SOCK_DGRAM).
+/// Returns fd on success, -1 on error. Socket is set NONBLOCK | CLOEXEC.
+int create_socket(int type);
+
+/// Bind a socket to a filesystem path.
+int bind_socket(int socket, const char *path);
+
+/// Bind a socket to an abstract namespace address.
+/// The abstract name does NOT include the leading null byte (it's added by this function).
+int bind_socket_abstract(int socket, const char *name);
+
+/// Listen for incoming connections (stream sockets only).
+int listen_socket(int socket, int backlog);
+
+/// Accept an incoming connection. Returns new fd on success, -1 on error.
+int accept_socket(int socket);
+
 void close_socket(int socket);
 
-static size_t c_msg_len(size_t datalen);
-static size_t c_msg_space(size_t datalen);
+/// Unlink a socket path from the filesystem.
+int unlink_socket_path(const char *path);
+
+// ---------------------------------------------------------------------------
+// Socket options
+// ---------------------------------------------------------------------------
+
+int get_socket_option(int socket, int level, int optname, int *value);
+int set_socket_option(int socket, int level, int optname, int value);
+
+// Convenience wrappers for common socket options
+int set_so_sndbuf(int socket, int size);
+int get_so_sndbuf(int socket);
+int set_so_rcvbuf(int socket, int size);
+int get_so_rcvbuf(int socket);
+int set_so_linger(int socket, int on_off, int linger_secs);
+
+// ---------------------------------------------------------------------------
+// Connection (client)
+// ---------------------------------------------------------------------------
+
+int create_unix_socket(const char *path);
+
+/// Connect a socket to a filesystem path.
+int connect_unix_socket(int socket, const char *path);
+
+int create_socketpair(int sv[2]);
+
+/// Connect a socket to an abstract namespace address.
+int connect_unix_socket_abstract(int socket, const char *name);
+
+// ---------------------------------------------------------------------------
+// Datagram (sendto / recvfrom)
+// ---------------------------------------------------------------------------
+
+/// Send data to a specific address on a datagram socket.
+/// Returns bytes sent or -1 on error.
+ssize_t send_to(int socket, const char *path, const void *data, size_t data_len);
+
+/// Send data to an abstract address on a datagram socket.
+ssize_t send_to_abstract(int socket, const char *name, const void *data, size_t data_len);
+
+/// Receive data from a datagram socket. Returns bytes received or -1.
+/// The sender address is NOT returned (lower-level).
+ssize_t recv_from(int socket, void *buffer, size_t length);
+
+// ---------------------------------------------------------------------------
+// Data send/recv (stream)
+// ---------------------------------------------------------------------------
 
 ssize_t send_bytes(int socket, const void *buffer, size_t length);
 ssize_t send_bytes_with_fd(int socket, int fd, const void *data, size_t data_len);
-
 ssize_t recv_bytes(int socket, void *buffer, size_t length);
 
+// ---------------------------------------------------------------------------
+// SCM_RIGHTS helpers
+// ---------------------------------------------------------------------------
+
+int recv_fd(int socket);
+void *unix_rights(int *fds, int num_fds, unsigned char *buf, size_t buflen);
+int parse_unix_rights(struct cmsghdr *cm, int *fds, int max_fds);
+size_t c_msg_len(size_t datalen);
+size_t c_msg_space(size_t datalen);
+
+// ---------------------------------------------------------------------------
+// Socket polling
+// ---------------------------------------------------------------------------
+
+int socket_has_data(int socket, int timeout);
