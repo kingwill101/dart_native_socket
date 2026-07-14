b6a3723a92d2029a538216bffeb1a480042ee806
kingwill101 <hey@glenfordwilliams.com>
2026-07-14 01:06:26 -0500

    Update build infrastructure for hooks 2.0.0 and programmatic ffigen
    - Migrate hook/build.dart to package:hooks 2.0.0 API (input/output)
    - Migrate to code_assets 1.0.0+, native_toolchain_c 0.19.0+
    - Replace YAML ffigen config with programmatic Dart script
    - Add tool/generate_bindings.dart using package:ffigen API

--- hook/build.dart
+++ hook/build.dart
@@ -2,22 +2,22 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:hooks/hooks.dart';
 import 'package:logging/logging.dart';
-import 'package:native_assets_cli/native_assets_cli.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
 
 void main(List<String> args) async {
-  await build(args, (config, output) async {
-    final packageName = config.packageName;
+  await build(args, (input, output) async {
+    final packageName = input.packageName;
     final cbuilder = CBuilder.library(
       name: packageName,
-      assetName: 'src/$packageName.dart',
+      assetName: '$packageName.dart',
       sources: [
         'src/$packageName.c',
       ],
     );
     await cbuilder.run(
-      config: config,
+      input: input,
       output: output,
       logger: Logger('')
         ..level = Level.ALL
---
+++ tool/generate_bindings.dart
@@ -0,0 +1,42 @@
+// Programmatic bindings generator using the `package:ffigen` Dart API.
+//
+// Usage:
+//   dart run tool/generate_bindings.dart
+//
+// This replaces the YAML-based `ffigen:` config in pubspec.yaml.
+
+import 'package:ffigen/ffigen.dart';
+
+void main() {
+  final ffigen = FfiGen();
+
+  final config = Config(
+    output: Uri.file('lib/src/native_socket.dart'),
+    entryPoints: [Uri.file('src/native_socket.h')],
+    // Only include declarations from our header, not system headers.
+    shouldIncludeHeaderFunc: (Uri header) =>
+        header.toFilePath().contains('native_socket.h'),
+    includeUnusedTypedefs: false,
+    sort: false,
+    useSupportedTypedefs: true,
+    ffiNativeConfig: const FfiNativeConfig(
+      enabled: true,
+      assetId: 'package:native_socket/native_socket.dart',
+    ),
+    compilerOpts: [
+      '-I/usr/lib/clang/22/include',
+      '-I/usr/local/include',
+      '-I/usr/include',
+    ],
+    functionDecl: DeclarationFilters.includeAll,
+    structDecl: DeclarationFilters.includeAll,
+    unionDecl: DeclarationFilters.includeAll,
+    enumClassDecl: DeclarationFilters.includeAll,
+    typedefs: DeclarationFilters.includeAll,
+    globals: DeclarationFilters.includeAll,
+    macroDecl: DeclarationFilters.excludeAll,
+    unnamedEnumConstants: DeclarationFilters.excludeAll,
+  );
+
+  ffigen.run(config);
+}
