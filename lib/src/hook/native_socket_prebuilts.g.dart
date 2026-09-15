0c7884def6705d6fdbed4dc65a441b6fbdb35fb6
kingwill101 <1114368+kingwill101@users.noreply.github.com>
2026-09-15 13:42:36 +0000

    chore: update prebuilt manifest [skip ci]

--- lib/src/hook/native_socket_prebuilts.g.dart
+++ lib/src/hook/native_socket_prebuilts.g.dart
@@ -5,30 +5,30 @@ import 'package:native_prebuilt/native_prebuilt.dart';
 
 const native_socketPrebuilts = PrebuiltManifest(
   schemaVersion: 1,
-  release: GitHubReleaseSource(owner: 'kingwill101', repository: 'dart_native_socket', tag: 'native_socket-v0.4.1'),
+  release: GitHubReleaseSource(owner: 'kingwill101', repository: 'dart_native_socket', tag: 'native_socket-v0.4.2'),
   artifacts: {
     'linux-x64': PrebuiltArtifact(
       archiveName: 'native_socket-linux-x64.tar.gz',
-      archiveSha256: '0f90990fda235cc3d74d1dc27f7acae18e9b5ea7f2fbfd98792812927e4bcc87',
-      payloadSha256: '2463d82ec0051233280a9d69c2f9ea4d29babc4cb6cdaa757edb612350e1bbd0',
+      archiveSha256: 'e21d5b77ebc5e8dfb5d02143387465614998de17f305e122b4ef1a6ba19a5082',
+      payloadSha256: '4d786c96af2786439656fd3033f435ac2d98d62c5bddece474945dee22677993',
       payload: DynamicLibraryPayload(libraryStem: 'native_socket', acceptVersionedNames: true),
     ),
     'android-arm64': PrebuiltArtifact(
       archiveName: 'native_socket-android-arm64.tar.gz',
-      archiveSha256: 'a4643c157d2a5cb39fa2e48d8bf26078a5b59973e97888cadbd282cdaab5b519',
-      payloadSha256: 'ab09a0824b74a806bfca9880a4ea74b2fb5b0976bb5971116518f17332358878',
+      archiveSha256: '665ba419bb7f2cfd73468f04b9b0f3f269fc209c7a1c4e5d4e52da0b129d0221',
+      payloadSha256: 'a8e214e2683c661f3c94eb20de3ba864bec229d8d71d4e55ca1410595d938f89',
       payload: DynamicLibraryPayload(libraryStem: 'native_socket', acceptVersionedNames: true),
     ),
     'android-arm': PrebuiltArtifact(
       archiveName: 'native_socket-android-arm.tar.gz',
-      archiveSha256: '0b8c7c33f1a0cafc85245355454d038d6426a4e070b793cb0d21a6e891a3ec23',
-      payloadSha256: '1b50c0ce29c55811cf62144065d8347ab428483cdf4412591a99446cdf427f28',
+      archiveSha256: '725536e6eecca62262aba5678f4a4b8b371d80674d520850441168c79fd058ef',
+      payloadSha256: 'bdd48af696389088fe2521fd5339b7f0a1ee80ccd564a5dc93e10d792148840b',
       payload: DynamicLibraryPayload(libraryStem: 'native_socket', acceptVersionedNames: true),
     ),
     'android-x64': PrebuiltArtifact(
       archiveName: 'native_socket-android-x64.tar.gz',
-      archiveSha256: '87e0b782ec17e7224756b0bb4e79ba9d54c2e7af46988b5ca50dcac504e1cc00',
-      payloadSha256: '7aadac454feb93d6a3b8daad73f0f5a4893bf5b01986799d4af1958b12e115bd',
+      archiveSha256: 'bdea5638738e43319977357dc2d4ba9c6d67954bade28434ee9bc4af1de9ae38',
+      payloadSha256: '812cee0671d94906850c413923bc7cdc0b9ce9f423b57e796e8167ccfe3a959d',
       payload: DynamicLibraryPayload(libraryStem: 'native_socket', acceptVersionedNames: true),
     ),
   },
